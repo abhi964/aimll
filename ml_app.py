3aa098172e12baedbf2315e79019c52cea55a609
abhi964 <72250204+abhi964@users.noreply.github.com>
2022-08-23 00:14:20 +0530

    Update ml_app.py

--- ml_app.py
+++ ml_app.py
@@ -41,7 +41,7 @@ button = st.button("Predict")
 if user_input and button :
     # test_sample
     token=nlp(user_input)
-    token.pos
+    token.pos_
     #if token.ent_type_=="GPE":
      #   st.write("It is City")
    # if token.ent_type_=='PERSON':
